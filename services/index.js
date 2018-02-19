5172e4aa99d3edb70e766f87b790cacdfed451c4
lildesert <julien.bistoquet@gmail.com>
2018-02-19 23:03:51 +0100

    add CORS enabled API

---
+++ services/index.js
@@ -0,0 +1,18 @@
+const axios = require('axios')
+const {
+  send
+} = require('micro')
+const microCors = require('micro-cors')
+const cors = microCors({
+  allowMethods: ['GET']
+})
+const DOMAIN = 'https://api.binance.com'
+
+const handler = async function (req, res) {
+  const params = req.url
+  const path = `${DOMAIN}${params}`
+  const response = await axios(path)
+  send(res, 200, response.data)
+}
+
+module.exports = cors(handler)
